9020c4052a251b20126ebc3c996be8bbb7e805aa
Thanongsak1991 <54270060+Thanongsak1991@users.noreply.github.com>
2019-08-19 17:37:16 +0700

    Add files via upload

---
+++ storage/framework/views/ba0471f504d77b41b0fb213030e0bf4be442b9f6.php
@@ -0,0 +1,23 @@
+<?php $__env->startSection('title','This is Index'); ?>
+<?php $__env->startSection('content'); ?>
+<div class="container" style="margin-top:50px">
+  <h3>Fixed Navbar</h3>
+  <div class="row">
+    <div class="col-md-4">
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>    
+    </div>
+    <div class="col-md-4"> 
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>
+    </div>
+    <div class="col-md-4"> 
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p>
+      <p>A fixed navigation bar stays visible in a fixed position (top or bottom) independent of the page scroll.</p> 
+    </div>
+  </div>
+</div>
+
+<h1>Scroll this page to see the effect</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Apache24\htdocs\demo\resources\views/index.blade.php ENDPATH**/ ?>
